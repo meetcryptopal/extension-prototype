2c6f9dd44dd5f23a43d4eb8e71a6265adc05417d
Edmund Mai <edmundmai@gmail.com>
2018-03-28 12:10:34 -0400

    scrape linkedin feed for headline and profile path

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,26 @@
-console.log(`'Allo 'Allo! Content script`)
+// LinkedIn feed
+const LINKEDIN_HEADLINE_CLASS = ".identity-headline";
+const LINKEDIN_PROFILE_LINK_CLASS = ".profile-rail-card__actor-link";
+
+const ScrapeLinkedInFeed = () => {
+  console.log("LINKEDIN FEED DETECTED");
+
+  const headline = $(LINKEDIN_HEADLINE_CLASS).text();
+  const profilePath = $(LINKEDIN_PROFILE_LINK_CLASS).attr("href");
+
+  console.log("LINKEDIN HEADLINE: ", headline);
+  console.log("LINKEDIN PROFILE PATH: ", profilePath);
+};
+
+// routes
+const LINKEDIN_FEED_URL = "https://www.linkedin.com/feed/";
+
+const ROUTE_ACTIONS = {
+  [LINKEDIN_FEED_URL]: ScrapeLinkedInFeed
+};
+
+$(function() {
+  console.log("CRYPTOPAL LOADED");
+  const action = ROUTE_ACTIONS[location.href];
+  action ? action() : null;
+});
