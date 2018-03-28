fbaa1fc8cadefdc7e8308fc2a8edb390496f3f1d
Edmund Mai <edmundmai@gmail.com>
2018-03-28 12:20:28 -0400

    separate into linkedin.js file

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,22 +1,8 @@
-// LinkedIn feed
-const LINKEDIN_HEADLINE_CLASS = ".identity-headline";
-const LINKEDIN_PROFILE_LINK_CLASS = ".profile-rail-card__actor-link";
-
-const ScrapeLinkedInFeed = () => {
-  console.log("LINKEDIN FEED DETECTED");
-
-  const headline = $(LINKEDIN_HEADLINE_CLASS).text();
-  const profilePath = $(LINKEDIN_PROFILE_LINK_CLASS).attr("href");
-
-  console.log("LINKEDIN HEADLINE: ", headline);
-  console.log("LINKEDIN PROFILE PATH: ", profilePath);
-};
-
 // routes
 const LINKEDIN_FEED_URL = "https://www.linkedin.com/feed/";
 
 const ROUTE_ACTIONS = {
-  [LINKEDIN_FEED_URL]: ScrapeLinkedInFeed
+  [LINKEDIN_FEED_URL]: LinkedIn.scrapeFeed
 };
 
 $(function() {
