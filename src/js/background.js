b94c436876aebe57b80da6eec082d345bf72442a
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-03-29 14:50:22 -0400

    Migrate history listener to new infrastructure

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,12 @@
 import "../img/icon-128.png";
 import "../img/icon-34.png";
+
+// TODO: Use chrome's history event instead.
+chrome.tabs.onUpdated.addListener((_tabId, changeInfo, tab) => {
+  const { status } = changeInfo;
+  const { url } = tab;
+
+  if (status === "complete" && url && url.startsWith("http")) {
+    console.log("CRYPTOPAL: ", url);
+  }
+});
