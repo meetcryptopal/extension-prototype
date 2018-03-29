5d53ebf47b1a957bd1337e14eec663b23fa43580
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-03-28 22:49:13 -0400

    Successful FB injection

--- src/js/services/facebook.js
+++ src/js/services/facebook.js
@@ -3,7 +3,18 @@ import $ from "jquery";
 export default () => {
   // TODO: This should be filtered from manifest.json
   if (!location.href.toLowerCase().includes("facebook.com")) return;
-  console.log("FACEBOOK");
+  $(() => {
+    // The page loads w/ JS, so can't rely on $(document).ready
+    const asyncLoadInterval = window.setInterval(() => {
+      console.log("attaching...");
+      const $likeLinks = $("a.UFILikeLink").on("click", () => {
+        console.log(`SOMEONE LIKED: ${$(this)}`);
+      });
 
-  console.log($);
+      if ($likeLinks.length > 0) {
+        clearInterval(asyncLoadInterval);
+        console.log("Cryptopal successfully injected.");
+      }
+    }, 1000);
+  });
 };
