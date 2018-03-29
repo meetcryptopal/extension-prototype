dbe3920a1a80df9e17d03bd59aa000b9dd667f82
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-03-28 23:33:55 -0400

    Prints some stuff

--- src/js/services/facebook.js
+++ src/js/services/facebook.js
@@ -6,14 +6,25 @@ export default () => {
   $(() => {
     // The page loads w/ JS, so can't rely on $(document).ready
     const asyncLoadInterval = window.setInterval(() => {
-      console.log("attaching...");
-      const $likeLinks = $("a.UFILikeLink").on("click", () => {
-        console.log(`SOMEONE LIKED: ${$(this)}`);
-      });
+      console.log("attaching!!...");
+      const $likeLinks = $("a.UFILikeLink");
 
       if ($likeLinks.length > 0) {
         clearInterval(asyncLoadInterval);
         console.log("Cryptopal successfully injected.");
+
+        $likeLinks.on("click", e => {
+          const $this = $(this);
+          console.log(JSON.stringify(this));
+          console.log(JSON.stringify($this.text()));
+          console.log(JSON.stringify($this.hasClass("UFILikeLink")));
+          console.log("---------  ");
+          const $contentWrapper = $this.closest(".userContentWrapper");
+          const $title = $contentWrapper.find("h5");
+
+          console.table($title);
+          console.log($title.text());
+        });
       }
     }, 1000);
   });
