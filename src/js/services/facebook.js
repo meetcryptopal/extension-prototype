eb067e102f5d707b47f9e7e7a3b660aa2b30b496
Edmund Mai <edmundmai@gmail.com>
2018-03-29 13:26:58 -0400

    facebook likes

--- src/js/services/facebook.js
+++ src/js/services/facebook.js
@@ -3,29 +3,14 @@ import $ from "jquery";
 export default () => {
   // TODO: This should be filtered from manifest.json
   if (!location.href.toLowerCase().includes("facebook.com")) return;
-  $(() => {
-    // The page loads w/ JS, so can't rely on $(document).ready
-    const asyncLoadInterval = window.setInterval(() => {
-      console.log("attaching!!...");
-      const $likeLinks = $("a.UFILikeLink");
-
-      if ($likeLinks.length > 0) {
-        clearInterval(asyncLoadInterval);
-        console.log("Cryptopal successfully injected.");
-
-        $likeLinks.on("click", e => {
-          const $this = $(this);
-          console.log(JSON.stringify(this));
-          console.log(JSON.stringify($this.text()));
-          console.log(JSON.stringify($this.hasClass("UFILikeLink")));
-          console.log("---------  ");
-          const $contentWrapper = $this.closest(".userContentWrapper");
-          const $title = $contentWrapper.find("h5");
-
-          console.table($title);
-          console.log($title.text());
-        });
-      }
-    }, 1000);
-  });
+  window.onload = () => {
+    $("body").on("click", ".UFILikeLink", e => {
+      console.log("LIKE BUTTON CLICKEDZZZZ");
+      const comment = $(e.target)
+        .closest("div.UFICommentContentBlock")
+        .find(".UFICommentBody")
+        .text();
+      console.log(comment);
+    });
+  };
 };
