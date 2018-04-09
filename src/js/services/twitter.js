93c5880469fbd2ad34c0cddbb828cc4d28ae50e2
Edmund Mai <edmundmai@gmail.com>
2018-04-09 11:51:39 -0400

    twitter poster username and profile link

--- src/js/services/twitter.js
+++ src/js/services/twitter.js
@@ -12,16 +12,31 @@ const trackLike = () => {
 
   const TWITTER_LIKE_BUTTON_SELECTOR =
     ".ProfileTweet-actionButton.js-actionFavorite";
+  const TWITTER_POST_HEADER_SELECTOR = ".stream-item-header";
   const TWITTER_POST_FOOTER_SELECTOR = ".stream-item-footer";
-  const TWITTER_POST_CONTENT_SELECTOR = ".js-tweet-text-container";
+  const TWITTER_POST_BODY_SELECTOR = ".js-tweet-text-container";
+  const TWITTER_POST_USERNAME_SELECTOR = ".fullname";
+  const TWITTER_POST_PROFILE_LINK_SELECTOR = ".js-user-profile-link";
 
   $("body").on("click", TWITTER_LIKE_BUTTON_SELECTOR, e => {
     const $likeBtn = $(e.target);
-    const body = $likeBtn
+
+    const $header = $likeBtn
       .closest(TWITTER_POST_FOOTER_SELECTOR)
-      .siblings(TWITTER_POST_CONTENT_SELECTOR)
-      .text();
-    console.log(body);
+      .siblings(TWITTER_POST_HEADER_SELECTOR);
+    const $body = $likeBtn
+      .closest(TWITTER_POST_FOOTER_SELECTOR)
+      .siblings(TWITTER_POST_BODY_SELECTOR);
+
+    const username = $header.find(TWITTER_POST_USERNAME_SELECTOR).text();
+    const profileLink = $header
+      .find(TWITTER_POST_PROFILE_LINK_SELECTOR)
+      .attr("href");
+    const content = $body.text();
+
+    console.log(content);
+    console.log(username);
+    console.log(profileLink);
   });
 };
 
