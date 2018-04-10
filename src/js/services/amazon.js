1a6cd61450e5da3d6082f0abc9f3f3868a249744
Edmund Mai <edmundmai@gmail.com>
2018-04-10 18:00:34 -0400

    store orders and cart items for amazon

--- src/js/services/amazon.js
+++ src/js/services/amazon.js
@@ -1,10 +1,33 @@
 import $ from "jquery";
+import store from "store";
+
+const saveOnOrder = () => {
+  const AMAZON_CHECKOUT_URL =
+    "https://www.amazon.com/gp/buy/spc/handlers/display.html";
+
+  const isCheckout = location.href.match(AMAZON_CHECKOUT_URL);
+
+  if (!isCheckout) {
+    return;
+  }
+  console.log("AMAZON ORDER DETECTED");
+
+  const AMAZON_ORDER_BUTTON_SELECTOR = ".place-your-order-button";
+
+  $("body").on("click", AMAZON_ORDER_BUTTON_SELECTOR, e => {
+    store.update("amazon", amazon => {
+      amazon.orders = amazon.orders || [];
+      amazon.orders = [...amazon.orders, ...amazon.cartItems];
+      amazon.cartItems = [];
+    });
+    console.log("AMAZON: ", store.get("amazon"));
+  });
+};
 
 const scrapeCart = () => {
-  const AMAZON_CART_URL =
-    "https://www.amazon.com/gp/cart/view.html/ref=lh_cart";
+  const AMAZON_CART_URL = "https://www.amazon.com/gp/cart/view.html";
 
-  const isCart = location.href === AMAZON_CART_URL;
+  const isCart = location.href.match(AMAZON_CART_URL);
 
   if (!isCart) {
     return;
@@ -16,21 +39,32 @@ const scrapeCart = () => {
   const AMAZON_PRODUCT_QUANTITY_SELECTOR = ".a-dropdown-prompt";
   const AMAZON_PRODUCT_PRICE_SELECTOR = ".sc-product-price";
 
-  $(AMAZON_PRODUCT_CONTAINER_SELECTOR).each((i, element) => {
+  const cartItems = $.map($(AMAZON_PRODUCT_CONTAINER_SELECTOR), element => {
     const productName = $(element)
       .find(AMAZON_PRODUCT_NAME_SELECTOR)
-      .text();
+      .text()
+      .trim();
     const productQuantity = $(element)
       .find(AMAZON_PRODUCT_QUANTITY_SELECTOR)
-      .text();
+      .text()
+      .trim();
     const productPrice = $(element)
       .find(AMAZON_PRODUCT_PRICE_SELECTOR)
-      .text();
+      .text()
+      .trim();
 
     console.log("AMAZON PRODUCT NAME:", productName);
     console.log("AMAZON PRODUCT QUANTITY:", productQuantity);
     console.log("AMAZON PRODUCT PRICE:", productPrice);
+    return {
+      productName,
+      productQuantity,
+      productPrice
+    };
   });
+
+  store.set("amazon", { cartItems });
+  console.log("AMAZON: ", store.get("amazon"));
 };
 
 export default {
