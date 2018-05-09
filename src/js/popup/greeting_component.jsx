6dd540b579ca8cbb0b194d4ed767e3bce0ee5913
Aaron Vasquez <aaronvasquez1@gmail.com>
2018-05-09 19:41:40 -0400

    Adds delete button

--- src/js/popup/greeting_component.jsx
+++ src/js/popup/greeting_component.jsx
@@ -3,6 +3,8 @@ import FileSaver from "file-saver";
 
 import icon from "../../img/icon-128.png";
 
+const store = window.chrome.storage.sync;
+
 const styles = {
   container: {
     margin: "20px 10px 20px 10px",
@@ -18,22 +20,27 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
+  },
+  btn: {
+    flex: "1"
   }
 };
 
 export default class extends React.Component {
   downloadData() {
-    window
-      .chrome
-      .storage
-      .sync
-      .get(null, data => {
-        console.log("DATA: ", data);
-        const exportedData = data || {};
-        const dataJson = JSON.stringify(data);
-        var dataBlob = new Blob([dataJson], {type: "text/plain;charset=utf-8"});
-        FileSaver.saveAs(dataBlob, "cryptopal.txt");
-      });
+    store.get(null, data => {
+      console.log("DATA: ", data);
+      const exportedData = data || {};
+      const dataJson = JSON.stringify(data);
+      var dataBlob = new Blob([dataJson], { type: "text/plain;charset=utf-8" });
+      FileSaver.saveAs(dataBlob, "cryptopal.txt");
+    });
+  }
+
+  deleteData() {
+    store.clear(() => {
+      console.log("Delete successful");
+    });
   }
 
   render() {
@@ -41,7 +48,12 @@ export default class extends React.Component {
       <div style={styles.container}>
         <h1 style={styles.heading}>CryptoPal</h1>
         <div style={styles.content}>
-          <button onClick={this.downloadData}>Download Data</button>
+          <button style={styles.btn} onClick={this.downloadData}>
+            Download Data
+          </button>
+          <button style={styles.btn} onClick={this.deleteData}>
+            Delete Data
+          </button>
         </div>
       </div>
     );
